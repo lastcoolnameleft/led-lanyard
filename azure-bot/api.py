1f9a282054826ebbedcf4d6e038fb7a2c43f7695
Tommy Falgout <tommy@lastcoolnameleft.com>
2019-06-06 15:30:03 +0000

    refactor pulling off of sb

--- azure-bot/api.py
+++ azure-bot/api.py
@@ -3,19 +3,19 @@ from flask import Flask
 from azure.servicebus import QueueClient, Message
 
 queue_client = QueueClient.from_connection_string(os.getenv('SERVICEBUS_CONN_STRING'), 'request')
-queue_receiver = queue_client.get_receiver()
 app = Flask(__name__)
 
-def get_command_servicebus(queue_receiver):
-    messages = queue_receiver.fetch_next(timeout=3)
-    if messages:
-        for message in messages:
-            print(message)
-            message.complete()
-            return str(message)
-    else:
-        print("The queue is empty")
-        return ''
+def get_command_servicebus(queue_client):
+    with queue_client.get_receiver() as queue_receiver:
+        messages = queue_receiver.fetch_next(timeout=3)
+        if messages:
+            for message in messages:
+                print(message)
+                message.complete()
+                return str(message)
+        else:
+            print("The queue is empty")
+            return ''
 
 @app.route("/")
 def hello():
@@ -23,5 +23,5 @@ def hello():
 
 @app.route("/pull")
 def pull():
-    return get_command_servicebus(queue_receiver)
+    return get_command_servicebus(queue_client)
 
